bf1a2464d5388b30c2a1adf14ab13f24e7b89545
FarrelSZ <farel.it12@gmail.com>
2025-12-05 09:27:03 +0700

    refactor: remove unused imports from utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { NxCamera } from "./nxapi";
-import { ICamera } from "@/types/Device";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
