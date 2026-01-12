984ccff981deb0031a984586a0b83877243d8223
FarrelSZ <farel.it12@gmail.com>
2026-01-12 16:18:48 +0700

    feat: add central export for monitoring module service functions

---
+++ src/components/monitoring/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Central export for monitoring module
+ */
+
+// Service functions
+export {
+  fetchSystemDetails,
+  fetchAllSystemDetails,
+  fetchServerLocations,
+  saveServerLocation,
+  deleteServerLocation,
+  openInGoogleMaps,
+  type SystemInfoData,
+  type ServerLocationData,
+} from "./monitoring-service";
