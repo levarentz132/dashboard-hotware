8995bb988a67d2561a786de64cae192ccb3f3dcb
FarrelSZ <farel.it12@gmail.com>
2026-01-12 16:19:08 +0700

    feat: add storage barrel exports for types, service functions, utilities, and components

---
+++ src/components/storage/index.ts
@@ -0,0 +1,40 @@
+/**
+ * Storage barrel exports
+ */
+
+// Types
+export type { StorageStatusInfo, Storage, CloudSystem, StorageFormData } from "./types";
+
+export { defaultStorageFormData, STORAGE_TYPES } from "./types";
+
+// Service functions
+export {
+  fetchCloudSystems,
+  attemptAutoLogin,
+  loginToCloudSystem,
+  fetchCloudStorages,
+  createCloudStorage,
+  updateCloudStorage,
+  deleteCloudStorage,
+  fetchLocalStorages,
+  type FetchStoragesResult,
+} from "./storage-service";
+
+// Utilities
+export {
+  formatBytes,
+  formatBytesToGB,
+  parseGBToBytes,
+  getUsagePercentage,
+  calculateTotalStorage,
+  calculateTotalUsed,
+  calculateTotalFree,
+  countOnlineStorages,
+  getServerId,
+  getStatusColor,
+  getUsageColor,
+  getProgressColor,
+} from "./storage-utils";
+
+// Components
+export { default as StorageManagement } from "./StorageManagement";
